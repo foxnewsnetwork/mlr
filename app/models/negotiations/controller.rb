5831dc8c882154aa43a17a7def04f83021bd225b
Thomas Chen <foxnewsnetwork@gmail.com>
2013-05-07 10:30:59 -0700

    * started down the process, but unfortunately can't do it until I can fully mock controllers   outside the controllers folder

--- app/models/negotiations/controller.rb
+++ app/models/negotiations/controller.rb
@@ -3,5 +3,9 @@ module Negotiations
     respond_to :js, :json, :html
     expose(:negotiation) { Negotiations.from_listing_and_buyer params[:listing_id], params[:buyer_id] }
     expose(:discussion) { Negotiations.from_discussion_id params[:discussion_id] }
+
+    def create
+      respond_with negotiation
+    end
   end
 end
--- spec/models/negotiations/controller_spec.rb
+++ spec/models/negotiations/controller_spec.rb
@@ -2,6 +2,24 @@ require 'spec_helper'
 
 describe Negotiations::Controller do
   describe "#create" do
-    
+    let(:create) { post :create, :listing_id => @listing.listing_id, :buyer_id => @buyer.company_id }
+    context "mocked record" do
+      before :each do
+        # @listing = ChineseFactory.listing
+        # @buyer = ChineseFactory.company
+        # FakeRecord.any_instance.should_receive(:listing).and_return(@listing)
+        # FakeRecord.any_instance.should_receive(:buyer).and_return(@buyer)
+        # Negotiations::Company.record_model = FakeRecord
+        # Negotiations::Listing.record_model = FakeRecord
+        # Negotiations::Discussion.record_model = FakeRecord
+      end
+      it "should create a proper discussion" do
+        pending "cannot pass this because rails expects me to put controllers in their own folder"
+        create
+        controller.discussion.should be_a Negotiations::Discussion
+        controller.discussion.buyer.should eq @buyer
+        controller.discussion.listing.should eq @listing
+      end
+    end
   end
 end
